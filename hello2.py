587fb61d3edfc751dde7e9172f4ccfb7c4b1fbbb
Seungyoung Woo <sywoo72@gmail.com>
2024-09-13 16:04:47 +0900

    4th commit

--- hello2.py
+++ hello2.py
@@ -1,2 +1,21 @@
 print("hello")
 print("hello")
+width = 20
+height = width * 2
+sq_meter = width * height
+print(sq_meter)
+print(5**2)
+arr = [1,2,3,4,5]
+print(arr[0])
+print(arr[-4])
+print(arr[-4:])
+arr2 = [6,7,8,9]
+arr3 = arr+arr2
+print(arr3)
+arr3[0] = 0
+print(arr3)
+a,b=0,1
+while a<100:
+    print(a,end=',')
+    a,b = b,a+b
+    
